a27e0fd81f7454129900e69ea1cb1749dc4f8a76
Buzut <blog@buzut.fr>
2018-06-28 16:31:09 +0200

    update lib for Winston@3.x & add standard dev files

--- README.md
+++ README.md
@@ -1,31 +1,52 @@
 # winston-log2gelf
-A [Graylog2](https://www.graylog.org/) or [GELF](http://docs.graylog.org/en/latest/pages/gelf.html) transport for [Winston](https://github.com/flatiron/winston). Supports HTTP(S) & TCP/TCP over TLS protocols.
+A [Graylog2](https://www.graylog.org/) or [GELF](http://docs.graylog.org/en/latest/pages/gelf.html) transport for [Winston@3.x](https://github.com/flatiron/winston). Supports HTTP(S) & TCP/TCP over TLS protocols.
+
+If you're looking for the 1.x version supporting Winston < 3.x, check [winston-log2gelf@1.9.1](https://github.com/Buzut/winston-log2gelf/tree/v1.9.1).
 
 ## Installation
-As it's written in ES6, this module requires at least node v4. Version `1.9.1` being the latest supporting Winston < 3.x, let's install this one.
-
 ``` sh
-  $ npm install --save winston-log2gelf@1.9.1
+  $ npm install --save winston-log2gelf
 ```
 
 ## Usage
 ```javascript
   const winston = require('winston');
-  require('winston-log2gelf');
+  const Log2gelf = require('winston-log2gelf');
 
-  const logger = new (winston.Logger)({
+  const logger = winston.createLogger({
     transports: [
         new (winston.transports.Console)({
             level: 'info',
-            handleExceptions: true,
-            humanReadableUnhandledException: true
+            handleExceptions: true
         }),
-        new (winston.transports.Log2gelf)({
+        new Log2gelf({
+            level: 'error',
+            host: '192.168.0.15',
+            port: 12201,
+            protocol: 'tls'
+        })
+    ]
+});
+```
+
+Note that if you wish to handle Exceptions, as [Winston automatically exists after an exception](https://github.com/winstonjs/winston#to-exit-or-not-to-exit), you have to disable the exit behaviour to let `Log2gelf` enough time to send the log across the network.
+
+```javascript
+  const logger = winston.createLogger({
+    exitOnError: false, // disable default winston exit
+    transports: [
+        new (winston.transports.Console)({
             level: 'info',
+            handleExceptions: true
+        }),
+        new Log2gelf({
+            level: 'error',
             host: '192.168.0.15',
             port: 12201,
             protocol: 'tls',
-            handleExceptions: true
+            handleExceptions: true, // handle exception within Log2gelf
+            exitOnError: true, // exit after exception has been sent
+            exitDelay: 1000 // leave Log2gelf 1sec to send the message
         })
     ]
 });
@@ -47,8 +68,14 @@ logger.transports.log2gelf.end();
 * `wait`: Milliseconds to wait between reconnect attempts (default 1000)
 * `level`: Level of messages this transport should log. See [winston levels](https://github.com/winstonjs/winston#logging-levels) (default: info)
 * `silent`: Boolean flag indicating whether to suppress output. (default: false)
-* `handleExceptions`: Boolean flag, whenever to handle uncaught exceptions. (default: false)
-* `service`: as facility is depreacated, service describes what kind of "service" this is (like MySQLd or Apache2). (default: nodejs)
+* `handleExceptions`: Boolean flag, whether to handle uncaught exceptions. (default: false)
+* `exitOnerror`: Will exit after x ms (2 sec by default) if Winston `exitOnError` is set to `false` if an exception is caught
+* `exitDelay`: Specify the exit delay in ms for `exitOnerror` option. (default 2000ms)
+* `service`: as facility is depreacated, service describes what kind of "service" this is (like MySQLd or Apache2). (default: nodejs* `service`: as facility is depreacated, service describes what kind of "service" this is (like MySQLd or Apache2). (default: nodejs* `service`: as facility is depreacated, service describes what kind of "service" this is (like MySQLd or Apache2). (default: nodejs* `service`: as facility is depreacated, service describes what kind of "service" this is (like MySQLd or Apache2). (default: nodejs)
 * `environment`: the environment on which your service is running. (default: development)
 * `release`: the version of your service (e.g. 1.0.0).
 * `_foo`: any underscore-prefixed custom option will be passed as is to the server.
+
+## Contributing
+There's sure room for improvement, so feel free to hack around and submit PRs!
+Please just follow the style of the existing code, which is [Airbnb's style](http://airbnb.io/javascript/) with [minor modifications](.eslintrc).
---
+++ index.js
@@ -0,0 +1,194 @@
+const os = require('os');
+const net = require('net');
+const secNet = require('tls');
+const http = require('http');
+const https = require('https');
+const Transport = require('winston-transport');
+
+class Log2gelf extends Transport {
+    constructor(options) {
+        super(options);
+
+        this.name = options.name || 'log2gelf';
+        this.hostname = options.hostname || os.hostname();
+        this.host = options.host || '127.0.0.1';
+        this.port = options.port || 12201;
+        this.protocol = options.protocol || 'tcp';
+        this.reconnect = options.reconnect || '0';
+        this.wait = options.wait || 1000;
+        this.exitOnError = options.exitOnError || false;
+        this.exitDelay = options.exitDelay || 2000;
+        this.service = options.service || 'nodejs';
+        this.level = options.level || 'info';
+        this.silent = options.silent || false;
+        this.environment = options.environment || 'development';
+        this.release = options.release;
+        this.customPayload = {};
+
+        Object.keys(options).forEach((key) => {
+            if (key[0] === '_') this.customPayload[key] = options[key];
+        });
+
+        // set protocol to use
+        if (this.protocol === 'tcp' || this.protocol === 'tls') {
+            const tcpGelf = this.sendTCPGelf();
+            this.send = tcpGelf.send;
+            this.end = tcpGelf.end;
+        }
+
+        else if (this.protocol === 'http' || this.protocol === 'https') this.send = this.sendHTTPGelf(this.host, this.port, false);
+        else throw new TypeError('protocol shoud be one of the following: tcp, tls, http or https');
+    }
+
+    /**
+     * Parse winston level as a string and return its equivalent numeric value
+     * @param { String }
+     * @return {int} level
+     */
+    levelToInt(level) { // eslint-disable-line
+        if (level === 'error') return 0;
+        if (level === 'warn') return 1;
+        if (level === 'info') return 2;
+        if (level === 'verbose') return 3;
+        if (level === 'debug') return 4;
+        if (level === 'silly') return 5;
+
+        return 0;
+    }
+
+    /**
+     * Open a TCP socket and return a logger funtion
+     * @return { Function } logger – logger(JSONlogs)
+     */
+    sendTCPGelf() {
+        const options = {
+            host: this.host,
+            port: this.port,
+            rejectUnauthorized: false
+        };
+
+        // whether or not tls is required
+        let clientType;
+        if (this.protocol === 'tls') clientType = secNet;
+        else clientType = net;
+
+        const client = clientType.connect(options);
+
+        client.on('connect', () => {
+            console.log('Connected to Graylog server');
+            client.reconnect = 0;
+        });
+
+        client.on('end', () => {
+            console.log('Disconnected from Graylog server');
+        });
+
+        client.on('error', (err) => {
+            console.error('Error connecting to Graylog:', err.message);
+            client.reconnect = client.reconnect + 1 || 0;
+        });
+
+        client.on('close', () => {
+            if (!client.ended && (this.reconnect === -1 || client.reconnect < this.reconnect)) {
+                client.timeout_id = setTimeout(() => {
+                    client.timeout_id = null;
+                    client.connect(options);
+                }, this.wait);
+            }
+        });
+
+        return {
+            send(msg) {
+                client.write(`${msg}\0`);
+            },
+            end() {
+                if (client.timeout_id) {
+                    clearTimeout(client.timeout_id);
+                }
+                client.ended = true;
+                client.end();
+            }
+        };
+    }
+
+    /**
+     * Set HTTP(S) connection and return logger function
+     * @return { Function } logger – logger(JSONlogs)
+     */
+    sendHTTPGelf() {
+        const options = {
+            port: this.port,
+            hostname: this.host,
+            path: '/gelf',
+            method: 'POST',
+            rejectUnauthorized: false
+        };
+
+        let clientType;
+        if (this.protocol === 'https') clientType = https;
+        else clientType = http;
+
+        return (msg) => {
+            options.headers = {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': Buffer.byteLength(msg)
+            };
+
+            const req = clientType.request(options, (res) => { // eslint-disable-line
+                // usefull for debug
+                // console.log('statusCode: ', res.statusCode);
+            });
+
+            req.on('error', (e) => {
+                console.error('Error connecting to Graylog:', e.message);
+            });
+
+            req.write(msg);
+            req.end();
+        };
+    }
+
+    /**
+     * Handle log message
+     * @param { Object } info – log object
+     * @param { Function } callback
+     */
+    log(info, callback) {
+        setImmediate(() => {
+            this.emit('logged', info);
+        });
+
+        const msg = info.message.split('\n')[0];
+
+        const meta = {};
+        Object.keys(info).forEach((key) => {
+            if (key !== 'error' && key !== 'level') meta[key] = info[key];
+        });
+
+        const payload = {
+            timestamp: Math.floor(Date.now() / 1000),
+            level: this.levelToInt(info.level),
+            host: this.hostname,
+            short_message: msg,
+            full_message: JSON.stringify(meta, null, 2),
+            _service: this.service,
+            _environment: this.environment,
+            _release: this.release
+        };
+
+        const gelfMsg = Object.assign({}, payload, this.customPayload);
+        this.send(JSON.stringify(gelfMsg));
+
+        // as we can't know when tcp is sent, delay cb for 2secs
+        if (info.exception && this.exitOnError) {
+            setTimeout(() => {
+                this.end();
+                process.exit(1);
+            }, this.exitDelay);
+        }
+
+        callback();
+    }
+}
+
+module.exports = Log2gelf;
